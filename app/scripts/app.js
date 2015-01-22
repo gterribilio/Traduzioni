d12b8c1b6d46cacf129c1aa9cb0889f63f31a709
unknown <terribilio.gi@CED34.izsto.it>
2015-01-22 15:48:26 +0100

    'f'

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name lemietraduzioniApp
+ * @description
+ * # lemietraduzioniApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('lemietraduzioniApp', []);
